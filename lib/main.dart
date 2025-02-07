da4e6bc007fb2238f6a45b86239498b0d2b67ae8
Gem Ryan <gembencito@gmail.com>
2025-02-08 04:37:37 +0800

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -17,7 +19,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 43, 80, 167)),
         ),
         home: MyHomePage(),
       ),
@@ -38,20 +40,57 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;   
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
+      body: Center( //refactor Column (wrap with center)
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //Text('A random idea:'),
+            //Text(appState.current.asLowerCase),
+            BigCard(pair: pair), //refactor (extract widget to create new class BigCard)
+            
+            
+            SizedBox(height: 10), //add space between BigCard and button
+        
+        
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-        ],
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context); //get app current theme
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary, //text size and color
+      );
+
+    return Card( //refactored Padding (wrap with widget)
+      color: theme.colorScheme.primary, //card color
+      child: Padding( //refactored Text (wrap with padding)
+        padding: const EdgeInsets.all(20.0),
+        child: Text(pair.asLowerCase, 
+        style: style,
+        semanticsLabel: "${pair.first} ${pair.second}", //for screenreader to read two words
+        ),
       ),
     );
   }
